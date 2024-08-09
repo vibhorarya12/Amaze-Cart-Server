92d6d519e484f461bbfa8f986794b8dcc3bc68ca
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-08-09 13:16:22 +0530

    ratings feild updated

--- models/products.model.js
+++ models/products.model.js
@@ -1,24 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  id: { type: Number, },
-  title: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String, required: true },
-  images: { type: [String], required: true },
-  creationAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  category: {
-    id: { type: Number,  },
-    name: { type: String, required: true },
-    image: { type: String, required: true },
+const productSchema = new mongoose.Schema(
+  {
+    id: { type: Number },
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
+    images: { type: [String], required: true },
     creationAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
-  }
-}, { collection: 'Products' });
+    rating: { type: Number, default: 3.5 },
+    category: {
+      id: { type: Number },
+      name: { type: String, required: true },
+      image: { type: String, required: true },
+      creationAt: { type: Date, default: Date.now },
+      updatedAt: { type: Date, default: Date.now },
+    },
+  },
+  { collection: "Products" }
+);
 
-productSchema.index({ title: 'text', 'category.name': 'text' });
+productSchema.index({ title: "text", "category.name": "text" });
 
-const Products = mongoose.model('Products', productSchema);
+const Products = mongoose.model("Products", productSchema);
 
 module.exports = Products;
