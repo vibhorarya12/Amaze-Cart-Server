d6270dc751105e93d438df7d40e6960826dcd923
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-08 23:42:31 +0530

    update address route added

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,9 +1,11 @@
 const {userController} = require('../controllers/index')
+const {auth} = require('../middleware');
 
 const router = require('express').Router();
 
 router.post('/checkExisting', userController.checkExisting);
 router.post('/registerUser', userController.registerUser);
 router.post('/loginUser', userController.loginUser);
+router.put('/updateAddress', auth, userController.updateAddress)
 
 module.exports = router;
